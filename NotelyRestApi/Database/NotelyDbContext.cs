01515e2d27787b34c521e21f11b0f5a6747d5789
Zcreator4 <zander.jimenez@hgs.hiddengeniusproject.org>
2020-10-14 08:52:36 -0700

    git commit

---
+++ NotelyRestApi/Database/NotelyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NotelyRestApi.Controllers.Models;
+
+namespace NotelyRestApi.Database
+{
+    public class NotelyDbContext :DbContext
+    {
+        public NotelyDbContext(DbContextOptions<NotelyDbContext> options) : base(options) { }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ NotelyRestApi/Migrations/20200930013132_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NotelyRestApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Subject = table.Column<string>(nullable: true),
+                    Detail = table.Column<string>(nullable: true),
+                    LastModified = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
